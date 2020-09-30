8ecae62aff45e9fad4d9536cbf8f3fc19488c2b4
Samson Lasode <samflicks@gmail.com>
2020-09-30 09:09:24 +0100

    Completed module

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
+import {fetchWithTimeout} from './services'
+
+const movies = require('./data/movies.json')
+
 export default function fetchMovies(){
-    
+    const resolveFunction = () => movies;
+  return  fetchWithTimeout(1000)
+   .then(resolveFunction)
 }
+
+const moviePromise = fetchMovies();
+moviePromise
+.then((results) => {
+    console.log(results)
+})
